86622e9a5bbe7eada3e8c77fdd62c3b1792573d4
dotun <oyelakindotun@gmail.com>
2020-10-26 19:13:02 +0100

    initial commit to production branch

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
 
-# Create your tests here.
+# import json
+
+# from django.urls import reverse
+# from rest_framework import status
+# from rest_framework.authtoken.models import Token
+# from rest_framework.test import APITestCase
+
+# from .models import Tank, User
+# from .serializers import TankSerializer, UserSerializer
